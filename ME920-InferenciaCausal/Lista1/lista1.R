2b5dd493dcf1632f58be605a90383ea49b6ba75f
Felipe de Albuquerque Marques <f236106@dac.unicamp.br>
2024-03-09 22:54:28 -0300

    update lista1.R ex 3

--- ME920-InferenciaCausal/Lista1/lista1.R
+++ ME920-InferenciaCausal/Lista1/lista1.R
@@ -3,3 +3,34 @@
 ################################################################################
 
 
+### Exercício 2
+## b)
+
+
+### Exercício 3
+library(Matching)
+library(combinat)
+
+data("lalonde")
+str(lalonde)
+head(lalonde)
+
+combn(colnames(lalonde)[-c(9, 12)], 2) 
+
+## Criando as combinações
+combinacoes <- list()
+
+for (i in 1:10) {
+  message(paste0(i, "/10"))
+  combinacoes <- append(combinacoes, combn(colnames(lalonde)[-c(9, 12)], i))
+}
+
+## Ajustando as regressões
+
+
+lm(re78 ~ ., data=lalonde)
+
+
+
+
+
