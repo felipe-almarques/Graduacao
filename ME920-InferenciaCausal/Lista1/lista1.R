daf58b756a15e600b532b9e14a14ddf3d8f71775
Felipe de Albuquerque Marques <f236106@dac.unicamp.br>
2024-03-11 20:07:12 -0300

    update ex2 and ex 3

--- ME920-InferenciaCausal/Lista1/lista1.R
+++ ME920-InferenciaCausal/Lista1/lista1.R
@@ -5,6 +5,33 @@
 
 ### Exercício 2
 ## b)
+library(mvtnorm)
+library(tidyverse)
+
+mu <- c(0, 0, 0)
+sigma <- matrix(c(1, .6, .4,
+                  .6, 1, .2,
+                  .4, .2, 1), byrow = T, nrow = 3)
+
+dados <- data.frame(rmvnorm(100, mu, sigma))
+colnames(dados) <- c("X", "Y", "Z")
+
+dados %>% 
+  ggplot() +
+  geom_density(aes(X), fill="blue", alpha = .3) +
+  geom_density(aes(Y), fill="red", alpha = .3) +
+  geom_density(aes(Z), fill="green", alpha = .3) +
+  labs(x = "", y = "Densidade") +
+  theme_bw()
+
+pyz <- cor(dados$Y, dados$Z)
+pxy <- cor(dados$X, dados$Y)
+pzx <- cor(dados$Z, dados$X)
+
+pyz_x <- (pyz - (pxy*pzx)) / (sqrt(1 - pxy^2) * sqrt(1 - pzx^2)) 
+
+pyz # 0.08362862
+pyz_x # -0.2000845
 
 
 ### Exercício 3
@@ -29,6 +56,11 @@ for (i in 1:10) {
 
 
 lm(re78 ~ ., data=lalonde)
+
+
+
+### Exercpicio 5
+
 
 
 
